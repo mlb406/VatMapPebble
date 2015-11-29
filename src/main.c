fe5090137337b6c891b4aca0946ccb1c792f21f4
mlb406 <mlb406@gmail.com>
2015-11-29 16:54:40 +0000

    Initial

---
+++ src/main.c
@@ -0,0 +1,324 @@
+#include <pebble.h>
+#include "main.h"
+
+#define KEY_GW 0
+#define KEY_SS 1
+#define KEY_KK 2
+#define KEY_LL 3
+#define KEY_GD 4
+#define KEY_CC 5
+#define KEY_PH 6
+#define KEY_LON 7
+
+#define GND 1
+#define TWR 2
+#define APP 3
+
+static Window *window;
+static BitmapLayer *background_layer;
+static GBitmap *background_bitmap;
+static Layer *airports_layer;
+static TextLayer *lon_layer, *time_layer;
+
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *t = localtime(&temp);
+  
+  char buffer[] = "00:00";
+  if (clock_is_24h_style() == true) {
+    strftime(buffer, sizeof(buffer), "%H:%M", t);
+  } else {
+    strftime(buffer, sizeof(buffer), "%H:%M", t);
+  }
+  text_layer_set_text(time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+  if(tick_time->tm_min % 15 == 0) {
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    dict_write_uint8(iter, 0, 0);
+    app_message_outbox_send();
+  }
+}
+
+static void update_airports() {
+  layer_mark_dirty(window_get_root_layer(window));
+}
+
+static void airports_create_proc(Layer *layer, GContext *ctx) {
+  GPoint SS = GPoint(115, 118);
+  GPoint GW = GPoint(106, 118);
+  GPoint LL = GPoint(102, 126);
+  GPoint CC = GPoint(80, 86);
+  GPoint KK = GPoint(110, 132);
+  GPoint GD = GPoint(78, 128);
+  GPoint PH = GPoint(66, 22);
+  
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  
+  graphics_fill_circle(ctx, GW, 1);
+  graphics_fill_circle(ctx, SS, 1);
+  graphics_fill_circle(ctx, LL, 1);
+  graphics_fill_circle(ctx, KK, 1);
+  graphics_fill_circle(ctx, GD, 1);
+  graphics_fill_circle(ctx, PH, 1);
+  graphics_fill_circle(ctx, CC, 1);
+  
+  if (persist_read_int(KEY_GW) > 0) {
+    #ifdef PBL_PLATFORM_BASALT 
+      switch(persist_read_int(KEY_GW)) {
+        case 1:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+          break;
+        case 2:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
+          break;
+        case 3:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 170, 0));
+          break;
+      }
+    #endif
+    graphics_fill_circle(ctx, GW, 1);
+  }
+  
+  if (persist_read_int(KEY_SS) > 0) {
+    #ifdef PBL_PLATFORM_BASALT 
+      switch(persist_read_int(KEY_SS)) {
+        case 1:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+          break;
+        case 2:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
+          break;
+        case 3:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 170, 0));
+          break;
+      }
+    #endif
+    graphics_fill_circle(ctx, SS, 1);
+  }
+  
+  if (persist_read_int(KEY_KK) > 0) {
+    #ifdef PBL_PLATFORM_BASALT 
+      switch(persist_read_int(KEY_KK)) {
+        case 1:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+          break;
+        case 2:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
+          break;
+        case 3:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 170, 0));
+          break;
+      }
+    #endif
+    graphics_fill_circle(ctx, KK, 1);
+  }
+  
+  if (persist_read_int(KEY_LL) > 0) {
+    #ifdef PBL_PLATFORM_BASALT 
+      switch(persist_read_int(KEY_LL)) {
+        case 1:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+          break;
+        case 2:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
+          break;
+        case 3:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 170, 0));
+          break;
+      }
+    #endif
+    graphics_fill_circle(ctx, LL, 1);
+  }
+  
+  if (persist_read_int(KEY_GD) > 0) {
+    #ifdef PBL_PLATFORM_BASALT 
+      switch(persist_read_int(KEY_GD)) {
+        case 1:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+          break;
+        case 2:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
+          break;
+        case 3:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 170, 0));
+          break;
+      }
+    #endif
+    graphics_fill_circle(ctx, GD, 1);
+  }
+  
+  if (persist_read_int(KEY_CC) > 0) {
+    #ifdef PBL_PLATFORM_BASALT 
+      switch(persist_read_int(KEY_CC)) {
+        case 1:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+          break;
+        case 2:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
+          break;
+        case 3:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 170, 0));
+          break;
+      }
+    #endif
+    graphics_fill_circle(ctx, CC, 1);
+  }
+  
+  if (persist_read_int(KEY_PH) > 0) {
+    #ifdef PBL_PLATFORM_BASALT 
+      switch(persist_read_int(KEY_PH)) {
+        case 1:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+          break;
+        case 2:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 0, 0));
+          break;
+        case 3:
+          graphics_context_set_fill_color(ctx, GColorFromRGB(255, 170, 0));
+          break;
+      }
+    #endif
+    graphics_fill_circle(ctx, PH, 1);
+  }
+  
+  char lon_buffer[256];
+  char buffer[256];
+  
+  if (persist_read_string(KEY_LON, lon_buffer, sizeof(lon_buffer)) != 0) {
+    persist_read_string(KEY_LON, lon_buffer, sizeof(lon_buffer));
+    strcpy(buffer, "LON: ");
+    strcat(buffer, lon_buffer);
+    text_layer_set_text(lon_layer, buffer);
+  }
+   
+}
+
+static void main_window_load() {
+  background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MAP_BG);
+  background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(background_layer, background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(background_layer));
+  
+  airports_layer = layer_create(GRect(0, 0, 144, 168));
+  layer_set_update_proc(airports_layer, airports_create_proc);
+  layer_add_child(window_get_root_layer(window), airports_layer);
+  
+  lon_layer = text_layer_create(GRect(0, 130, 144, 20));
+  text_layer_set_text_color(lon_layer, GColorBlack);
+  text_layer_set_background_color(lon_layer, GColorClear);
+  text_layer_set_text_alignment(lon_layer, GTextAlignmentLeft);
+  text_layer_set_font(lon_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(lon_layer));
+  
+  time_layer = text_layer_create(GRect(0, -5, 140, 30));
+  text_layer_set_text_alignment(time_layer, GTextAlignmentRight);
+  text_layer_set_text_color(time_layer, GColorFromRGB(255, 0, 0));
+  text_layer_set_background_color(time_layer, GColorClear);
+  text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(time_layer, "00:00");
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
+}
+
+static void main_window_unload() {
+  gbitmap_destroy(background_bitmap);
+  bitmap_layer_destroy(background_layer);
+  layer_destroy(airports_layer);
+  text_layer_destroy(lon_layer);
+  text_layer_destroy(time_layer);
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  Tuple *t = NULL;
+  
+  t = dict_read_first(iterator);
+  while (t != NULL) {
+    switch (t->key) {
+      case KEY_GW:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_GW");
+        persist_write_int(KEY_GW, t->value->int16);
+        break;
+      case KEY_SS:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_SS");
+        persist_write_int(KEY_SS, t->value->int16);
+        break;
+      case KEY_KK:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_KK");
+        persist_write_int(KEY_KK, t->value->int16);
+        break;
+      case KEY_LL:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_LL");
+        persist_write_int(KEY_LL, t->value->int16);
+        break;
+      case KEY_GD:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_GD");
+        persist_write_int(KEY_GD, t->value->int16);
+        break;
+      case KEY_CC:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_CC");
+        persist_write_int(KEY_CC, t->value->int16);
+        break;
+      case KEY_PH:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_PH");
+        persist_write_int(KEY_PH, t->value->int16);
+        break;
+      case KEY_LON:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Got KEY_LON");
+        persist_write_string(KEY_LON, t->value->cstring);
+        break;
+      default:
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Unknown key :(");
+        break;
+    }
+    t = dict_read_next(iterator);
+  }
+  update_airports();
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void init() {
+  window = window_create();
+  
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  
+  window_stack_push(window, true);
+  
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  
+  update_time();
+}
+
+static void deinit() {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+  return 0;
+}
---
+++ src/pebble-js-app.js
@@ -0,0 +1,126 @@
+var UK_URL = 'http://api.vateud.net/online/atc/eg,lon,essex,thames,solent.json';
+var GW = 0;
+var SS = 0;
+var KK = 0;
+var LL = 0;
+var GD = 0;
+var CC = 0;
+var PH = 0;
+var LON = "";
+
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function getOnline() {
+  xhrRequest(UK_URL, "GET", function(response) {
+    var data = JSON.parse(response);
+    
+    for (var i = 0; i < data.length; i++) {
+      var call = data[i].callsign;
+      var station = ((data[i].callsign).split("_"))[0];
+      var loc = ((data[i].callsign).split("_"))[1];
+      var pos = ((data[i].callsign).split("_"))[(((data[i].callsign).split("_")).length)-1];
+      
+      // stations available
+      // GW SS KK LL GD CC PH
+      if (station == "EGGW") {
+        if (pos == "GND" && GW < 1) {
+          GW = 1;
+        } else if (pos == "TWR" && GW < 2) {
+          console.log("TWR online!");
+          GW = 2;
+        } else if (pos == "APP" && GW < 3) {
+          GW = 3;
+        }
+      } else if (station == "EGSS") {
+        if (pos == "GND" && SS < 1) {
+          SS = 1;
+        } else if (pos == "TWR" && SS < 2) {
+          SS = 2;
+        } else if (pos == "APP" && SS < 3) {
+          SS = 3;
+        }
+      } else if (station == "EGKK") {
+        if (pos == "GND" && KK < 1) {
+          KK = 1;
+        } else if (pos == "TWR" && KK < 2) {
+          KK = 2;
+        } else if (pos == "APP" && KK < 3) {
+          KK = 3;
+        }
+      } else if (station == "EGLL") {
+        if (pos == "GND" && LL < 1) {
+          LL = 1;
+        } else if (pos == "TWR" && LL < 2) {
+          LL = 2;
+        } else if (pos == "APP" && LL < 3) {
+          LL = 3;
+        }
+      } else if (station == "EGGD") {
+        if (pos == "GND" && GD < 1) {
+          GD = 1;
+        } else if (pos == "TWR" && GD < 2) {
+          GD = 2;
+        } else if (pos == "APP" && GD < 3) {
+          GD = 3;
+        }
+      } else if (station == "EGCC") {
+        if (pos == "GND" && CC < 1) {
+          CC = 1;
+        } else if (pos == "TWR" && CC < 2) {
+          CC = 2;
+        } else if (pos == "APP" && CC < 3) {
+          CC = 3;
+        }
+      } else if (station == "EGPH") {
+        if (pos == "GND" && PH < 1) {
+          PH = 1;
+        } else if (pos == "TWR" && PH < 2) {
+          PH = 2;
+        } else if (pos == "APP" && PH < 3) {
+          PH = 3;
+        }
+      } else if (station == "ESSEX") {
+        GW = 3;
+        SS = 3;
+      } else if (station == "LON" && station != "LON_CTR") {
+        LON.concat(loc + ", ");
+      } else if (call == "LON_CTR") {
+        LON.concat("BB,");
+      }
+    }
+    
+    var dict = {
+      "KEY_GW": GW,
+      "KEY_SS": SS,
+      "KEY_KK": KK,
+      "KEY_LL": LL,
+      "KEY_GD": GD,
+      "KEY_CC": CC,
+      "KEY_PH": PH,
+      "KEY_LON": LON
+    };
+    
+    Pebble.sendAppMessage(dict, function(e) {
+      console.log("Success sending to Pebble");
+    }, function(e) {
+      console.log("Error sending to Pebble");
+    });
+    
+  });
+}
+
+Pebble.addEventListener("ready", function() {
+  console.log("PebbleKit JS ready!");
+  getOnline();
+});
+
+Pebble.addEventListener("appmessage", function() {
+  getOnline();
+});
